ce5b3c87e23089a54c8cfdab509d84ef7492bca4
Sidharth Nambiar <snambiar01@gmail.com>
2022-04-14 11:46:43 -0400

    added section 25

--- sections/24/DOM_Intro_CODE/Pokemon/app.js
+++ sections/24/DOM_Intro_CODE/Pokemon/app.js
@@ -3,8 +3,7 @@
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 898; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
     const label = document.createElement('span');
@@ -12,8 +11,8 @@ for (let i = 1; i <= 151; i++) {
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`
 
-
     pokemon.appendChild(newImg);
     pokemon.appendChild(label);
     container.appendChild(pokemon)
 }
+
