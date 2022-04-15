dc5d6cc5f4c6314c9551e0074ee24c83af957989
Sidharth Nambiar <snambiar01@gmail.com>
2022-04-15 09:30:15 -0400

    completed random color picker

--- sections/25/DOM_Events_CODE/EventBasics/app.js
+++ sections/25/DOM_Events_CODE/EventBasics/app.js
@@ -1,39 +1,93 @@
+// const btn = document.querySelector('#v2');
+
+// btn.onclick = function () {
+//     console.log("YOU CLICKED ME!")
+//     console.log("I HOPE IT WORKED!!")
+// }
+
+// function scream() {
+//     console.log("AAAAAHHHHH");
+//     console.log("STOP TOUCHING ME!")
+// }
+
+// btn.onmouseenter = scream;
+
+
+// document.querySelector('h1').onclick = () => {
+//     alert('you clicked the h1!')
+// }
+
+
+// const btn3 = document.querySelector('#v3');
+// btn3.addEventListener('click', function () {
+//     alert("CLICKED!");
+// })
+
+// function twist() {
+//     console.log("TWIST!")
+// }
+// function shout() {
+//     console.log("SHOUT!")
+// }
+
+// const tasButton = document.querySelector('#tas');
+
+// // tasButton.onclick = twist;
+// // tasButton.onclick = shout;
+
+// tasButton.addEventListener('click', twist)
+// tasButton.addEventListener('click', shout)
+
+
+
+//my code below
+
+
+console.log('linked')
+
 const btn = document.querySelector('#v2');
+console.dir(btn)
 
 btn.onclick = function () {
-    console.log("YOU CLICKED ME!")
-    console.log("I HOPE IT WORKED!!")
+    console.log('You clicked me!');
+    console.log("I hope it worked")
 }
 
 function scream() {
-    console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!")
+    console.log("Ahh!!")
 }
 
 btn.onmouseenter = scream;
 
-
-document.querySelector('h1').onclick = () => {
-    alert('you clicked the h1!')
+document.querySelector('h1').onclick = function () {
+    console.log("You clicked on H1")
 }
-
 
 const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
-    alert("CLICKED!");
+
+
+btn3.addEventListener('dblclick', function () {
+    console.log('you double clicked on button 3')
+    alert('button 3 double clicked')
 })
 
+
 function twist() {
-    console.log("TWIST!")
-}
-function shout() {
-    console.log("SHOUT!")
+    console.log("please twist")
 }
 
-const tasButton = document.querySelector('#tas');
+function shout() {
+    console.log("shouting!!!")
+}
+
+const tasButton = document.querySelector('#tas')
 
 // tasButton.onclick = twist;
 // tasButton.onclick = shout;
 
-tasButton.addEventListener('click', twist)
+tasButton.addEventListener('click', twist, { once: true})
 tasButton.addEventListener('click', shout)
+
+tasButton.addEventListener('click', (e) => {
+    console.log(e)
+})
--- sections/25/DOM_Events_CODE/RandomColors/app.js
+++ sections/25/DOM_Events_CODE/RandomColors/app.js
@@ -1,16 +1,39 @@
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
+// const button = document.querySelector('button');
+// const h1 = document.querySelector('h1');
 
-button.addEventListener('click', function () {
-    const newColor = makeRandColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
+// button.addEventListener('click', function () {
+//     const newColor = makeRandColor();
+//     document.body.style.backgroundColor = newColor;
+//     h1.innerText = newColor;
+// })
 
-const makeRandColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+// const makeRandColor = () => {
+//     const r = Math.floor(Math.random() * 255);
+//     const g = Math.floor(Math.random() * 255);
+//     const b = Math.floor(Math.random() * 255);
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// my code below
+
+function getColor() {
+  let eightBit = Math.floor(Math.random() * 255);
+  return eightBit;
 }
 
+const btn = document.querySelector("button");
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+
+body.style.display = 'flex'
+body.style.flexDirection = 'column';
+body.style.alignItems = 'center';
+
+
+
+btn.addEventListener("click", () => {
+  let color = `${getColor()},${getColor()},${getColor()}`;
+
+    body.style.backgroundColor = `rgb(${color})`;
+    h1.innerText = `rgb(${color})`
+});
